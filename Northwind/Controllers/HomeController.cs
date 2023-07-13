ae7cdfe11f4c726a22f17ec57a382b302da13111
Shadow0607 <40194318+Shadow0607@users.noreply.github.com>
2023-07-14 07:29:01 +0800

    1.connnect database 2.add getallshippers in homecontroller for select data

---
+++ Northwind/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using System.Data.SqlClient;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+
+namespace Northwind.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Home>>> GetAllShippers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Home> heroes = await SelectAllShippers(connection);
+            return Ok(heroes);
+        }
+        private static async Task<IEnumerable<Home>> SelectAllShippers(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Home>("select * from Shippers");
+        }
+    }
+}
+
---
+++ Northwind/Home.cs
@@ -0,0 +1,10 @@
+﻿namespace Northwind;
+
+public class Home
+{
+    public int ShipperID { get; set; }
+
+    public string? CompanyName { get; set; }
+    public string? Phone { get; set; }
+}
+

244dd48f07937cdfc560b96d256c356cad8088d7
Shadow0607 <40194318+Shadow0607@users.noreply.github.com>
2023-07-14 08:07:48 +0800

    Add update delete insert funtion

--- Northwind/Controllers/HomeController.cs
+++ Northwind/Controllers/HomeController.cs
@@ -30,6 +30,39 @@ namespace Northwind.Controllers
             IEnumerable<Home> heroes = await SelectAllShippers(connection);
             return Ok(heroes);
         }
+        [HttpGet("{ShipperID}")]
+        public async Task<ActionResult<Home>> GetShippers(int ShipperID)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryFirstAsync<Home>("select * from Shippers where ShipperID = @ShipperID",
+                    new { ShipperID = ShipperID });
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Home>>> CreateShippers(Home Shippers)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into  Shippers(CompanyName, Phone) values (@CompanyName, @Phone)", Shippers);
+            return Ok("Insert Database OK");
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Home>>> UpdateHero(Home Shippers)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update Shippers set CompanyName = @CompanyName, Phone = @Phone where ShipperID = @ShipperID", Shippers);
+            return Ok(await SelectAllShippers(connection));
+        }
+
+        [HttpDelete("{ShipperID}")]
+        public async Task<ActionResult<List<Home>>> DeleteHero(int ShipperID)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from Shippers where ShipperID = @ShipperID", new { ShipperID = ShipperID });
+            return Ok(await SelectAllShippers(connection));
+        }
+
         private static async Task<IEnumerable<Home>> SelectAllShippers(SqlConnection connection)
         {
             return await connection.QueryAsync<Home>("select * from Shippers");
